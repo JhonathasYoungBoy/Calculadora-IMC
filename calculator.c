e89d03f09338ea4411b5cc89911680804b65ef37
Jhonathas <jhonathas.souza351@gmail.com>
2021-07-15 23:52:38 -0300

    Calculadora Com Looping

--- calculator.c
+++ calculator.c
@@ -1,18 +1,28 @@
-int main(void){
+    #include<stdio.h>
+
+    int main(void){
 
       
      float imc, peso, altura;
+     int comeco, continua;
+     comeco = 1;
+     continua = 2;
      
-     printf("Digite 1 para calcularmos seu IMC. \n");
-     scanf("%f", &imc);
+        
+    while(2){
+            
+     printf("\nDigite 1 para calcularmos seu IMC. \n");
+     scanf("%d", &comeco);
      
      printf("Digite seu peso: \n");
      scanf("%f", &peso);
      
      printf("Digite sua altura: \n");
      scanf("%f", &altura);
+     
+    
        
-    if (imc == 1){
+    if (imc == 0){
      
         imc = peso / (altura * altura);
         
@@ -22,42 +32,53 @@ int main(void){
 
     if(imc <= 18.5){
       
-    printf("Seu IMC está muito baixo");
+    printf("Seu IMC esta muito baixo");
 
 }
    
     
     else if(imc <= 24.9){
 
-      printf("Seu IMC está Normal");
+      printf("Seu IMC esta Normal");
       
   } 
     
 
    else if(imc <= 29.9){
 
-      printf("Seu IMC está sobrepeso");
+      printf("Seu IMC esta sobrepeso");
     
   }
   
 
    else if(imc <= 34.9){
 
-      printf("Seu IMC está obsidade grau 1");
+      printf("Seu IMC esta obsidade grau 1");
   }
 
     else if(imc <= 39.9){
     
 
-      printf("Seu IMC está obsidade grau 2");
+      printf("Seu IMC esta obsidade grau 2");
   }
 
 
      else if(imc >= 40){
 
-      printf("Seu IMC está obsidade grau 3");
+      printf("Seu IMC esta obsidade grau 3");
+   
   }
-
-  
+        
+      else
+            
+         (continua == 2);
+        printf("\nDeseja repetir? Aperte 2. \n");
+        scanf("%d", &continua);
+        
+            
+        
+        
+}
+    
    return 0;
 }
